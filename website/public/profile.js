71af4d616f3ca043fea5f5e1653a9bf968924830
Antonio Ren <antonio.ren@epita.fr>
2020-01-07 14:18:41 +0100

    [Profile] - fix entreprise page

--- website/public/profile.js
+++ website/public/profile.js
@@ -90,17 +90,24 @@ class Profile extends React.Component {
                         <p className="card-text">{this.state.email}</p>
                     </div>
                 </div>
-                <h5 className="centrer">Tes candidatures</h5>
                 {
-                    this.state.register.length > 0 ? (
-                        this.state.register.map(item =>
-                            <Card item={item}
-                            />)
-                    ) : (
-                        <div class="card mt-2 col-sm">
-                            <div class="card-body">Inscrit à aucune offre</div>
-                        </div>
-                    )
+                    this.state.role === 'Candidat' ? (
+                        <div>
+                    <h5 className="centrer">Tes candidatures</h5>
+                        </div>) : (<div></div>)
+                }
+                {
+                    this.state.role === 'Candidat' ? (
+                        this.state.register.length > 0 && this.state.role === 'Candidat' ? (
+                            this.state.register.map(item =>
+                                <Card item={item}
+                                />)
+                        ) : (
+                            <div class="card mt-2 col-sm">
+                                <div class="card-body">Inscrit à aucune offre</div>
+                            </div>
+                        )
+                    ) : (<div></div>)
                 }
             </div >
         );
